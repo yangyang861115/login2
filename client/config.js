195c398b186e91e0c04d11cb4976524eed643abc
yangyang861115 <yangyang861115@gmail.com>
2016-06-20 17:51:47 -0700

    refactor config file

--- client/config.js
+++ client/config.js
@@ -4,8 +4,8 @@
 (function () {
     angular
         .module("myApp")
-        .config(configuration)
-        .run(restrict);
+        .config(configuration);
+        //.run(restrict);
 
     function configuration($routeProvider, $httpProvider) {
 
@@ -21,7 +21,10 @@
             .when("/dashboard", {
                 templateUrl: "views/dashboard/dashboard.view.html",
                 controller: "DashboardController",
-                controllerAs: "model"
+                controllerAs: "model",
+                resolve : {
+                    loggedIn: checkLoggedIn
+                }
             })
             .when("/lesson", {
                 templateUrl: "views/lesson/lesson.view.html",
@@ -41,6 +44,50 @@
             .otherwise({
                 redirectTo: "/"
             });
+
+        function  checkLoggedIn(Auth, User, $location, $q){
+            var deferred = $q.defer();
+            var token = Auth.getToken();
+            if(token) {
+                User.validateToken()
+                    .then(
+                        function(response){
+                            if(response && response.data.success) {
+                                deferred.resolve();
+                            } else {
+                                deferred.reject();
+                                $location.url('/login');
+                            }
+                        },
+                        function(err){
+                            deferred.reject();
+                            $location.url('/login');
+                        }
+                    );
+            } else {
+                var cookieToken = Auth.getTokenFromCookie();
+                if(cookieToken) {
+                    Auth.saveToken(cookieToken, function () {
+                        User.validateToken(cookieToken)
+                            .then(function(response){
+                                if (response && response.data.success) {
+                                    deferred.resolve();
+                                }
+                                else {
+                                    $window.localStorage.removeItem('jwtToken');
+                                    Auth.deleteRememberMeCookie();
+                                    deferred.reject();
+                                    $location.url('/login');
+                                }
+                            });
+                    });
+                } else {
+                    deferred.reject();
+                    $location.url('/login');
+                }
+            }
+            return deferred.promise;
+        }
 
         $httpProvider.interceptors.push(authInterceptor);
 
@@ -76,57 +123,57 @@
 
     }
 
-    function restrict($rootScope, $location, $window, Auth, User) {
-        $rootScope.$on("$routeChangeStart", function (event, next) {
-            if (!Auth.isAuthed()) {
-                console.log("You don't have a token in session storage");
-                if (next.templateUrl === "views/dashboard/dashboard.view.html") {
-                    $location.path("/login");
-                } else if (next.templateUrl === "views/login/login.view.html") {
-                    console.log("checking cookie..........");
-                    checkLoginAgain();
-                }
-            } else {
-                console.log("You  have a token in session storage");
-                if (next.templateUrl === "views/login/login.view.html") {
-                    $location.path("/dashboard");
-                }
-            }
-        });
-
-        function checkLoginAgain() {
-            var cookieState = Auth.validateRememberMeCookie();
-            if (cookieState) {
-                var token;
-                console.log("this is a valid cookie");
-                var cookielist = document.cookie.split(';');
-                for(var i in cookielist) {
-                    if(cookielist[i].indexOf('remember-me') != -1) {
-                        //get the token from the cookie list
-                        token = cookielist[i].split('=')[1];
-                    }
-                }
-
-                Auth.saveToken(token, function () {
-                    User.validateToken()
-                        .then(function(response){
-                            if (response && response.data.success) {
-                                console.log("login successfully by token in the cookie!");
-                                window.location = "#/dashboard";
-                            }
-                            else {
-                                $window.localStorage.removeItem('jwtToken');
-                                Auth.deleteRememberMeCookie();
-                                window.location = "#/login";
-                                console.log("The token in the cookie is invalid");
-                            }
-                        });
-                });
-
-            } else {
-                console.log("no cookie found or the cookie has expired");
-            }
-        }
-    }
+    //function restrict($rootScope, $location, $window, Auth, User) {
+    //    $rootScope.$on("$routeChangeStart", function (event, next) {
+    //        if (!Auth.isAuthed()) {
+    //            console.log("You don't have a token in session storage");
+    //            if (next.templateUrl === "views/dashboard/dashboard.view.html") {
+    //                $location.path("/login");
+    //            } else if (next.templateUrl === "views/login/login.view.html") {
+    //                console.log("checking cookie..........");
+    //                checkLoginAgain();
+    //            }
+    //        } else {
+    //            console.log("You  have a token in session storage");
+    //            if (next.templateUrl === "views/login/login.view.html") {
+    //                $location.path("/dashboard");
+    //            }
+    //        }
+    //    });
+    //
+    //    function checkLoginAgain() {
+    //        var cookieState = Auth.validateRememberMeCookie();
+    //        if (cookieState) {
+    //            var token;
+    //            console.log("this is a valid cookie");
+    //            var cookielist = document.cookie.split(';');
+    //            for(var i in cookielist) {
+    //                if(cookielist[i].indexOf('remember-me') != -1) {
+    //                    //get the token from the cookie list
+    //                    token = cookielist[i].split('=')[1];
+    //                }
+    //            }
+    //
+    //            Auth.saveToken(token, function () {
+    //                User.validateToken()
+    //                    .then(function(response){
+    //                        if (response && response.data.success) {
+    //                            console.log("login successfully by token in the cookie!");
+    //                            window.location = "#/dashboard";
+    //                        }
+    //                        else {
+    //                            $window.localStorage.removeItem('jwtToken');
+    //                            Auth.deleteRememberMeCookie();
+    //                            window.location = "#/login";
+    //                            console.log("The token in the cookie is invalid");
+    //                        }
+    //                    });
+    //            });
+    //
+    //        } else {
+    //            console.log("no cookie found or the cookie has expired");
+    //        }
+    //    }
+    //}
 
 })();
